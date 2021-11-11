4a7300302352af396a6b36751247c829d37b4817
mahesh m <maheshreddyreddy1887@gmail.com>
2021-11-11 01:24:56 -0500

    Displaying Refrigerator Names

---
+++ Samsung/src/main/java/com/xworkz/samsung/sevlets/Godrej.java
@@ -0,0 +1,45 @@
+package com.xworkz.samsung.sevlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Godrej extends HttpServlet {
+
+	public Godrej() {
+		System.out.println("created LG");
+
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String brand = request.getParameter("brand");
+		String color = request.getParameter("color");
+		String price = request.getParameter("price");
+
+		System.out.println("brand :" + brand);
+		System.out.println("color :" + color);
+		System.out.println("price :" + price);
+
+		response.setContentType("text/html");
+
+		PrintWriter printWriter = response.getWriter();
+		printWriter.write("<html>");
+		printWriter.write("<body>");
+		printWriter.write("save is successs");
+		printWriter.write("</body>");
+		printWriter.write("</html>");
+
+	}
+
+}
+
+
+
+
